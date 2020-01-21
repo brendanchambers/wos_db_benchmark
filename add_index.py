06a0b14d1026c240aa0b3ff4a280415a3320ea80
brendanchambers <brendanchambers@gmail.com>
2020-01-21 11:30:08 -0600

    .py for benchmarking

---
+++ add_index.py
@@ -0,0 +1,234 @@
+#import mysql.connector as mysql
+import pymysql
+import time
+import json
+
+master_timer_start = time.time()
+
+###################################################################
+client_config = {'unix_socket':'/project2/jevans/study_dbs/mysql/.sql.sock',
+                'database':'test_wos_cut_full'}
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+# print the table names
+sql_query = 'SHOW tables'
+cursor.execute(sql_query)
+
+for element in cursor:
+    print(element)
+
+cursor.close()
+db.close()
+
+
+###################################################################
+# add index to SOURCE field of publications
+
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE FULLTEXT INDEX pub_source_idx ON publications (source)'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+
+###################################################################
+# add index to wos_id field of publications
+
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX wos_id_idx ON publications (wos_id(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+###################################################################
+# add index to PUBYEAR field of publications
+
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX pub_year_idx ON publications (pubyear)'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+
+###################################################################
+# contributors:  first name, last name, wos_id
+
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX first_name_idx ON contributors (first_name(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+
+###################################################################
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX last_name_idx ON contributors (last_name(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+###################################################################
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX wos_id_idx ON contributors (wos_id(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+###################################################################
+# institutions - country, wos_id
+
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE FULLTEXT INDEX country_idx ON institutions (country(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+###################################################################
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX wos_id_idx ON institutions (wos_id(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+###################################################################
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX source_idx ON the_references (wos_id(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+###################################################################
+start_time = time.time()
+
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+
+sql_query = 'CREATE INDEX target_idx ON the_references (uid(50))'
+cursor.execute(sql_query)
+
+cursor.close()
+db.commit()
+db.close()
+
+end_time = time.time()
+print('elapsed s: {}'.format(end_time - start_time))
+
+
+###################################################################
+db = pymysql.connect(**client_config)
+cursor = db.cursor()
+sql_query = 'DESCRIBE test_wos'
+cursor.execute(sql_query)
+
+for el in cursor:
+    print(el)
+    
+###################################################################
+
+# TODO add indices as needed to speed up test queries
+
+db.close()
+
+###################################################################
+
+master_timer_stop = time.time()
+total_elapsed = master_timer_stop - master_timer_start
+print('total elapsed time during ingestion: {} s'.format(total_elapsed))
+
+timing_info = {'indexing_time': total_elapsed}
+with open('indexing_timer.json','w') as f:
+    json.dump(timing_info, f)
+               
+        
+        
---
+++ ingest.py
@@ -0,0 +1,97 @@
+import sqlalchemy
+import pandas as pd
+#import mysql.connector as mysql
+import pymysql
+import time
+import os
+import json
+
+master_timer_start = time.time()
+
+
+#########################################################################
+#      specifies how to recognize the partial file splits:
+
+data_dir = '/project2/jevans/study_dbs/data/full_v1/'
+
+pubs_substring = 'wos_cut_publications'  # postfixed with e.g. batch_0.csv
+contrib_substring = 'wos_cut_contributors'
+inst_substring = 'wos_cut_institutions'
+ref_path = 'wos_cut_refs.csv'
+wosIDs_path = 'wos_cut_wosids.csv'
+
+# sql alchemy connection string:
+db_name = 'test_wos_cut_full'
+connect_string = "mysql+pymysql:///{}?unix_socket=/project2/jevans/study_dbs/mysql/.sql.sock".format(db_name)
+
+
+#########################################################################
+# create the database
+
+client_config = {'unix_socket':'/project2/jevans/study_dbs/mysql/.sql.sock'}
+db = pymysql.connect(**client_config)
+
+
+
+cursor = db.cursor()
+sql = "CREATE DATABASE {}".format(db_name)
+try:
+    cursor.execute(sql)
+except Exception as e:
+    print(e)
+cursor.close()
+db.close()
+
+
+#########################################################################
+# build a dictionary for to keep track of file names, etc:
+
+tables = {}
+tables['publications'] = {'substring_filemarker':pubs_substring}
+tables['contributors'] = {'substring_filemarker':contrib_substring}
+tables['institutions'] = {'substring_filemarker':inst_substring}
+tables['the_references'] = {'substring_filemarker':ref_path}
+tables['wosIDs'] = {'substring_filemarker':wosIDs_path}
+
+
+#########################################################################
+# sort out the matching file splits
+
+all_datafiles = os.listdir(data_dir)
+
+for k, v in iter(tables.items()):
+    
+    print('processing {} ...'.format(k))
+    start_time = time.time()
+    
+    tables[k]['files'] = [f for f in all_datafiles \
+                          if (v['substring_filemarker'] in f)]
+    DFs = []
+    for filename in tables[k]['files']:
+        path = os.path.join(data_dir,filename)
+        #print(path)
+        DFs.append(pd.read_csv(path, sep=','))
+
+    # join dfs
+    df = pd.concat(DFs, axis=0, ignore_index=True)        
+
+    # insert into db
+    sql_engine = sqlalchemy.create_engine(connect_string)
+    df.to_sql(con=sql_engine,
+             name=k,
+             if_exists='replace',
+             chunksize=50000)
+        
+    end_time = time.time()
+    print('{} ingested in {} s'.format(k, end_time-start_time))
+
+    
+#########################################################################
+master_timer_stop = time.time()
+total_elapsed = master_timer_stop - master_timer_start
+print('total elapsed time during ingestion: {} s'.format(total_elapsed))
+
+timing_info = {'ingestion_time': total_elapsed}
+with open('ingestion_timer.json','w') as f:
+    json.dump(timing_info, f)
+               
